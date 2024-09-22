e84f91f234fcad05b96b9ffe6947e69e9dc574c7
Oleksandr <aleks6699@gmail.com>
2024-09-22 18:06:48 +0300

    fear: add task human

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+
+    const teens = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    const hundreds = "hundred";
+    if (number < 10) {
+        return ones[number];
+    }
+
+    if (number >= 10 && number < 20) {
+        return teens[number - 10];
+    }
+
+    if (number >= 20 && number < 100) {
+        const ten = Math.floor(number / 10);
+        const one = number % 10;
+        return tens[ten] + (one ? " " + ones[one] : "");
+    }
+
+    if (number >= 100 && number < 1000) {
+        const hundred = Math.floor(number / 100);
+        const remainder = number % 100;
+        const remainderReadable = remainder ? " " + toReadable(remainder) : "";
+        return ones[hundred] + " " + hundreds + remainderReadable;
+    }
+
+    return "";
+};
